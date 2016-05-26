4e4825abab009d8e3c96591b873b694664a49f0b
Alberto Lopez <albertolopez19@gmail.com>
2016-05-26 11:19:53 -0400

    Basic editing functionality

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,58 @@
 import React from 'react';
 
-export default({task}) => <div>{task}</div>;
+export default class Note extends React.Component {
+	constructor(props) {
+		super(props);
+
+		// Editing state
+		this.state = {
+			editing: false
+		};
+	}
+	render() {
+		if(this.state.editing) {
+			return this.renderEdit();
+		}
+
+		return this.renderNote();
+	}
+	renderEdit = () => {
+		return <input type="text"
+			ref={
+				element => element ?
+				element.selectionStart = this.props.task.length : null
+			}
+			autofocus={true}
+			defaultValue={this.props.task}
+			onBlur={this.finishEdit}
+			onKeyPress={this.checkEnter} />
+	};
+	renderNote = () => {
+		// trigger editing logic
+		return <div onClick={this.edit}>{this.props.task}</div>;
+	};
+	edit = () => {
+		// edit mode
+		this.setState({
+			editing: true
+		});
+	};
+	checkEnter = (e) => {
+		// user hits enter
+		if(e.key === 'Enter') {
+			this.finishEdit(e);
+		}
+	};
+	finishEdit = (e) => {
+		const value = e.target.value;
+
+		if(this.props.onEdit) {
+			this.props.onEdit(value);
+
+			// exit edit mode
+			this.setState({
+				editing: false
+			});
+		}
+	};
+}
