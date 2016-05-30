3106d52b84d2a6cc815f3ae5ec1baf1dc49f168b
Alberto Lopez <albertolopez19@gmail.com>
2016-05-29 21:33:27 -0400

    Add alt container logic

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,3 +1,4 @@
+import AltContainer from 'alt-container';
 import React from 'react';
 import Notes from './Notes.jsx';
 import NoteActions from '../actions/NoteActions.js';
@@ -5,46 +6,34 @@ import NoteStore from '../stores/NoteStore.js';
 
 export default class App extends React.Component {
 
-	constructor(props) {
-		super(props);
-
-		this.state = NoteStore.getState();
-	}
-	componentDidMount() {
-		NoteStore.listen(this.storeChanged);
-	}
-	componentWillUnmount() {
-		NoteStore.unlisten(this.storeChanged);
-	}
-	storeChanged = (state) => {
-		this.setState(state);
-	};
-	render() {
-		const notes = this.state.notes;
-		return (
-			<div>
-				<button className="add-note" onClick={this.addNote}>+</button>
-				<Notes
-					notes={notes}
-					onEdit={this.editNote}
-					onDelete={this.deleteNote}
-				/>
-			</div>
-		);
-	}
 	addNote() {
-		NoteActions.create({task: 'New Task'});
+		NoteActions.create({ task: 'New Task' });
 	}
 	editNote(id, task) {
-		if(!task.trim()) {
+		if (!task.trim()) {
 			return;
 		}
 
-		NoteActions.update({id, task});
+		NoteActions.update({ id, task });
 	}
 	deleteNote(id, e) {
 		e.stopPropagation();
 
 		NoteActions.delete(id);
+	}
+	render() {
+		return (
+			<div>
+				<button className="add-note" onClick={this.addNote}>+</button>
+				<AltContainer
+					stores={[NoteStore]}
+					inject={{
+						notes: () => NoteStore.getState().notes
+					}}
+				>
+					<Notes onEdit={this.editNote} onDelete={this.deleteNote}/>
+				</AltContainer>
+			</div>
+		);
 	}
 }
